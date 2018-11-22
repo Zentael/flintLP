4eade93bf16c580f3cea8bb0201e53ea4bdec1e8
Zentael <charlielucas8@gmail.com>
2018-11-22 11:16:31 +0100

    Update mentions_legales.php

--- mentions_legales.php
+++ mentions_legales.php
@@ -16,6 +16,68 @@
             <article>
                 <h2>Editeur du site</h2>
                 <p><b>Handi-cape</b></p>
+                <p>XX rue de Paris</p>
+                <p>XX000 Paris</p>
+                <p>France</p>
+                <p>Tel : + 33 (0)X XX XX XX</p>
+                <p>Url</p>
+                <br>
+                <p>Handi-cape est une société anonyme au capital de XXXX€</p>
+            </article>
+            <article>
+                <h2>Conditions d'utilisations</h2>
+                <p>Le site accessible par les url suivants : www.site-internet-qualite.fr est exploité dans le respect de la législation française. L'utilisation de ce site est régie par les présentes conditions générales. En utilisant le site, vous reconnaissez avoir pris connaissance de ces conditions et les avoir acceptées. Celles-ci pourront êtres modifiées à tout moment et sans préavis par la société Natural-net.
+                    Natural-net ne saurait être tenu pour responsable en aucune manière d’une mauvaise utilisation du service.
+                </p>
+            </article>
+            <article>
+                <h2>Informatique et liberté</h2>
+                <p>Les informations susceptibles d'être collectées sur le présent site sont exclusivement destinées au traitement de votre demande par handi-cape.extension. Vous disposez d'un droit d'accès, de modification, de rectification et de suppression des données qui vous concernent (art. 34 de la loi Informatique et Libertés). Plus d'informations sur www.cnil.fr
+                    Vous pouvez exercer ce droit en nous écrivant par mail //insérer l'adresse mail\\ ou à l'adresse suivante : Talentup, 83 avenue Charles de Gaulle, 92200 Neuilly sur Seine. Téléphone + 33 1 46 03 92 49
+                    Même si Handi-cape sa a mis en oeuvre des moyens pour protéger les informations personnelles des utilisateurs, la société ne peut garantir qu'elles ne soient récoltées de façon frauduleuse par un internautre malveillant.
+                </p>
+            </article>
+            <article>
+                <h2>Litiges</h2>
+                <p>Les présentes conditions sont régies par les lois françaises et toute contestation ou litiges qui pourraient naître de l'interprétation ou de l'exécution de celles-ci seront de la compétence exclusive des tribunaux dont dépend le siège social de la société Natural-net. La langue de référence, pour le règlement de contentieux éventuels, est le français.
+                </p>
+            </article>
+            <article>
+                <h2>Confidentialité</h2>
+                <p>Vos données personnelles sont confidentielles et ne seront en aucun cas communiquées à des tiers hormis pour la bonne exécution de la prestation.
+                </p>
+            </article>
+            <article>
+                <h2>Droits de propriété intelectuelle</h2>
+                <p>L'ensemble de ce site relève des législations françaises et internationales sur le droit d'auteur et la propriété intellectuelle. Tous les droits de reproduction sont réservés, y compris pour les documents iconographiques et photographiques.
+                </p>
+            </article>
+        </section>
+        <section>
+            <article>
+                <h2>Conditions d'utilisation du site</h2>
+                <p>En tant qu'utilisateur, vous acceptez le fait que l'utilisation du site Internet ou des informations qui y sont reprises se fait exclusivement à vos propres risques. Le site Internet et toutes les informations qu'il contient sont disponibles tels quels : "en l'état" et sans qu'aucune garantie quelconque soit fournie.</p>
+                <p>Handi-cape.extension consent tous les efforts raisonnables pour offrir des informations correctes et mises à jour sur le site Internet. Lorsque Handi-cape.extension est averti de la présence éventuelle d'erreurs ou d'inexactitudes sur leur site Internet, tous les moyens raisonnables sont mis en oeuvre pour y remédier aussi rapidement que faire se peut. Malgré tous nos efforts, il se peut que des erreurs et des méprises subsistent. Handi-cape.extension ne saurait en aucun cas garantir que le site Internet est mis à jour et ne contient aucune erreur à tout moment et dans toutes les conditions. C'est la raison pour laquelle vous ne pouvez, en tant qu'utilisateur, faire aveuglément confiance à toutes les informations reprises sur le site Internet. Il est dès lors préférable de les contrôler préalablement auprès de la tierce partie intéressée d'où proviennent lesdites informations.
+                </p>
+                <p>Handi-cape.extension ne saurait en aucun cas être tenue responsable de tout désavantage que vous pourriez encourir à la suite d'informations erronées, inexactes ou manquantes ou à la suite d'un manque de clarté sur le site Internet de Handi-cape.extension  ou sur un site Internet tiers auquel il est fait référence.
+                </p>
+                <p>De plus, vous acceptez le fait que le téléchargement d'informations ou l'obtention de quelque autre manière que ce soit d'informations à la suite de l'utilisation du site Internet se fait de votre propre chef et sous votre propre responsabilité.
+                </p>
+                <p>Vous êtes seul responsable des éventuels dommages causés à votre ordinateur et de la perte éventuelle de toute donnée qui résulterait du téléchargement d'informations.
+                </p>
+                <p>Nous attirons votre attention sur le fait que Handi-cape.extension ne joue aucun rôle quelconque dans le processus de recrutement et n'assume aucune responsabilité pour la forme, le contenu et les erreurs éventuelles dans les CV des utilisateurs enregistrés.
+                </p>
+                <p>En tant qu'utilisateur, vous acceptez, par conséquent, que</p>
+                <ul>
+                    <li>le site Internet Handi-cape.extension n'a pas pour objectif de satisfaire à la situation spécifique d'une personne physique ou morale ;</li>
+                    <li>la description d'un CV n'est pas nécessairement complète ou exhaustive </li>
+                    <li>il est parfois fait référence aux sites Internet de tiers sur lesquels Handi-cape n’a aucun contrôle et pour lesquels elle rejette, par conséquent, toute forme de responsabilité</li>
+                </ul>
+                <p>Par conséquent, vous ne pouvez tenir Handi-cape.extension pour responsable de tout désavantage résultant de l'utilisation du site Internet si vous n'avez pas pris les limitations susmentionnées en considération.</p>
+                <p>Handi-cape.extension ne vous concède qu'une autorisation de visualisation de son contenu à titre personnel et privé, à l'exclusion de toute visualisation ou diffusion publique. L'autorisation de reproduction ne vous est concédée que sous forme numérique sur votre ordinateur de consultation aux fins de visualisation des pages consultées par votre logiciel de navigation. L'impression papier est autorisée aux fins de copie privée à l'usage exclusif du copiste au sens de l'article L. 122-5 2 du Code de la Propriété Intellectuelle. La création d'un lien hypertexte vers une adresse du site nécessite l’accord préalable de talentup.com.</p>
+                <p>Malgré le soin apporté au traitement des informations, Talentup décline toute responsabilité concernant les erreurs ou omissions portant sur les informations diffusées sur ce site. Talentup ne peut être tenue responsable de l'interprétation des informations contenues dans ce site, ni des conséquences de leur utilisation.
+                    Aucune garantie n’est donnée que ce site sera disponible sans interruption et aucune responsabilité ne peut être acceptée concernant des pertes ou dommages résultant d’une indisponibilité du site.</p>
+                <p>Le présent Site a été créé en France. En utilisant le présent Site, vous acceptez les conditions d'utilisation décrites ci-avant, sans préjudice de tous recours de nature contractuelle ou délictuelle pouvant être exercés par talentup.com. Tout litige portant sur l'interprétation ou l'exécution d'un engagement contractuel prévu au présent site sera de la compétence exclusive des tribunaux français faisant application de la loi française.</p>
             </article>
         </section>
     </body>
